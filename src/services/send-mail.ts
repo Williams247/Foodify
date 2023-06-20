42af2599b3f113fd0fb92414b856ed45b7d5bd64
Williams Alex <godswillwilliams9@gmail.com>
2023-06-20 15:14:15 +0100

    Added other credentials to mail trap

--- src/services/send-mail.ts
+++ src/services/send-mail.ts
@@ -1,34 +1,31 @@
 import nodemailer from "nodemailer";
-import { MailtrapClient } from "mailtrap"
+import { MailtrapClient } from "mailtrap";
 import dotenv from "dotenv";
 import { SendMailProps } from "@utils";
 dotenv.config();
 
 export const sendEMail = async ({ mailTo, subject, text }: SendMailProps) => {
+  const client = new MailtrapClient({
+    endpoint: process.env.MAIL_TRAP_ENDPOINT,
+    token: process.env.TOKEN as string,
+  });
 
-const TOKEN = "2b16d9a51c3cdef5432f8a7482818d15";
-const ENDPOINT = "https://send.api.mailtrap.io/";
+  const sender = {
+    email: "mailtrap@mailtrap.personal",
+    name: "Mailtrap Test",
+  };
+  const recipients = [
+    {
+      email: "nodegmrv1@gmail.com",
+    },
+  ];
 
-const client = new MailtrapClient({ endpoint: ENDPOINT, token: TOKEN });
-
-const sender = {
-  email: "mailtrap@mailtrap.personal",
-  name: "Mailtrap Test",
-};
-const recipients = [
-  {
-    email: "nodegmrv1@gmail.com",
-  }
-];
-
-client
-  .send({
-    from: sender,
-    to: recipients,
-    subject: "You are awesome!",
-    text: "Congrats for sending test email with Mailtrap!",
-    category: "Integration Test",
-  })
-  .then(console.log, console.error);
-
+  client
+    .send({
+      from: sender,
+      to: recipients,
+      subject: "You are awesome!",
+      text: "Congrats for sending test email with Mailtrap!",
+      category: "Integration Test",
+    })
 };
