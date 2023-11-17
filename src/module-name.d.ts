40553191483e96e1b4efabff5612e58fe46d4d93
Williams Alex <godswillwilliams9@gmail.com>
2023-11-17 23:39:41 +0100

    Chore: Update

---
+++ src/module-name.d.ts
@@ -0,0 +1,19 @@
+declare module "express" {
+  export function func(): string;
+}
+
+declare module "jsonwebtoken" {
+  export function func(): string;
+}
+
+declare module "bcryptjs" {
+  export function func(): string;
+}
+
+declare module "body-parser" {
+  export function func(): string;
+}
+
+declare module "morgan" {
+  export function func(): string;
+}
